81145dfca8fa5594a356dcb5010310fa241b8b90
kimjinsan <lazybones1@naver.com>
2022-10-24 13:43:53 +0900

    [FEAT] Kotlin webView Init
    코틀린 웹뷰 작성
    url 임시로 네이버로 사용

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webview.settings.apply {
+            this.setSupportMultipleWindows(false) // 새창 띄우기 허용
+            this.setSupportZoom(false) // 화면 확대 허용
+            this.javaScriptEnabled = true // 자바스크립트 허용
+            this.javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
+            this.loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+            this.useWideViewPort = true // html의 viewport 메타 태그 지원
+            this.builtInZoomControls = false // 화면 확대/축소 허용
+            this.displayZoomControls = false
+            this.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN // 컨텐츠 사이즈 맞추기
+            this.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
+            this.domStorageEnabled = true // 로컬 저장 허용
+            this.databaseEnabled = true
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            }
+        }
+
+        webview.webViewClient = WebViewClient()
+
+        webview.loadUrl("https://www.naver.com")
+    }
+
+    override fun onBackPressed() {
+        if (webview.canGoBack())
+        {
+            webview.goBack()
+        }
+        else
+        {
+            finish()
+        }
+    }
+}

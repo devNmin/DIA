c512214f8b423a4e2c9bcda398801dfb4873433b
윤영훈 <rod3539@hanmail.net>
2022-10-25 15:19:24 +0900

    [FEAT] mainpage 수정
    mainpage 브랜치 생성

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 
 export default function MainPage() {
+  const [modalIsOpen, setModalIsOpen] = useState(true)
   return (
     <div>
-      This is mainPage
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+      This is Modal content
+      <button onClick={()=> setModalIsOpen(false)}>Modal close</button>
+      </Modal>
+      <button>
+        전술보드 들어가기
+      </button>
+
+      <button>
+        마이페이지
+      </button>
+
     </div>
   )
 }

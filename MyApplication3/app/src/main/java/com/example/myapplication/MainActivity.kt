7af4f48a90c8dbb60a40810862ff77bc2d921995
kimjinsan <lazybones1@naver.com>
2022-10-27 11:11:53 +0900

    [FEAT] Android - Javascript 통신
    캠 전환용 플로팅 버튼 주석처리
    자바스크립트 - ReactAlert.showToast(문자열) 시 토스트 메시지 발생

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.myapplication
 
 import android.os.Build
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(){
 
@@ -14,6 +15,13 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //플로팅 버튼
+//        fab.setOnClickListener{
+//            val intent= Intent( this, CamActivity::class.java)
+//            startActivity(intent)
+//        }
+
         webview.settings.apply {
             this.setSupportMultipleWindows(false) // 새창 띄우기 허용
             this.setSupportZoom(false) // 화면 확대 허용
@@ -21,21 +29,22 @@ class MainActivity : AppCompatActivity(){
             this.javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
             this.loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
             this.useWideViewPort = true // html의 viewport 메타 태그 지원
-            this.builtInZoomControls = false // 화면 확대/축소 허용
-            this.displayZoomControls = false
+            this.builtInZoomControls = true // 화면 확대/축소 허용
+            this.displayZoomControls = true
             this.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN // 컨텐츠 사이즈 맞추기
             this.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
             this.domStorageEnabled = true // 로컬 저장 허용
             this.databaseEnabled = true
-
+            this.lightTouchEnabled = true
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             }
         }
 
         webview.webViewClient = WebViewClient()
+        webview.addJavascriptInterface(WebAppInterface(this), "ReactAlert")
 
-        webview.loadUrl("https://www.naver.com")
+        webview.loadUrl("https://k7b307.p.ssafy.io/")
     }
 
     override fun onBackPressed() {

0896aeb2deb6cc2495c14f169fd69758beec4591
kimjinsan <lazybones1@naver.com>
2022-10-27 19:22:41 +0900

    [FEAT] 하드웨어 가속 추가

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -29,13 +29,13 @@ class MainActivity : AppCompatActivity(){
             this.javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
             this.loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
             this.useWideViewPort = true // html의 viewport 메타 태그 지원
-            this.builtInZoomControls = true // 화면 확대/축소 허용
-            this.displayZoomControls = true
+            this.builtInZoomControls = false // 화면 확대/축소 허용
+            this.displayZoomControls = false
             this.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN // 컨텐츠 사이즈 맞추기
             this.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
             this.domStorageEnabled = true // 로컬 저장 허용
             this.databaseEnabled = true
-            this.lightTouchEnabled = true
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             }
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity(){
         webview.webViewClient = WebViewClient()
         webview.addJavascriptInterface(WebAppInterface(this), "ReactAlert")
 
-        webview.loadUrl("https://k7b307.p.ssafy.io/")
+        webview.loadUrl("https://k7b307.p.ssafy.io/canvasTest")
     }
 
     override fun onBackPressed() {

80b5f3e57b1ab7f73a1eabc3cabe222f7cd30ca9
seulgiL <seulki199@gmail.com>
2022-10-27 17:52:36 +0900

    [FEAT]스크롤, 드래그 금지해봄

--- frontend/src/components/Drawing/DrawingTool.js
+++ frontend/src/components/Drawing/DrawingTool.js
@@ -1,7 +1,7 @@
 //https://velog.io/@mokyoungg/React-React%EC%97%90%EC%84%9C-Canvas-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0%EB%A7%88%EC%9A%B0%EC%8A%A4-%EA%B7%B8%EB%A6%AC%EA%B8%B0
 
 import React, { useEffect, useRef, useState } from 'react';
-
+import styles from './DrawingTool.module.css';
 function DrawingTool() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
@@ -55,7 +55,7 @@ function DrawingTool() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.size}>
       <input
         type="color"
         id="brushColor"

2c7ba5330cd5ef069cd25f4c9d40fbfe43fcbccb
hyeoklimKwon <khlzzang@daum.net>
2022-10-31 11:19:26 +0900

    [FEAT] UserContext created

--- frontend/src/pages/IpInsertPage.js
+++ frontend/src/pages/IpInsertPage.js
@@ -1,22 +1,24 @@
-import React, {useRef} from 'react'
+import React, {useRef, useContext} from 'react'
 import './IpInsertPage.css'
 import InputMask from "react-input-mask";
-
+import UserContext from '../context/UserContext';
+import { useHistory } from 'react-router-dom';
 
 
 
 export default function IpInsertPage() {
+  const { setIpV4 } = useContext(UserContext)
+  const history = useHistory();
   const ipAddress = useRef();  
   const ipChecker = () => {
     const ipsubmit = ipAddress.current.value;
     console.log(ipsubmit);
-  }
-
-    
+    setIpV4(ipsubmit)
+    history.push('/canvasTest')
+  }    
   return (
     <div>
-      <h1>노트북 ip를 입력해주세요</h1>
-  
+      <h1>노트북 ip를 입력해주세요</h1>  
         <div>    
             <InputMask
             className= 'ipInput'

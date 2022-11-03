415b480a83965df1e559e89527df414ab99de4ed
kimjinsan <lazybones1@naver.com>
2022-11-03 13:29:33 +0900

    [FEAT] 좌표 정보 받기 api

--- backend/src/main/java/com/ssafy/backend/dto/UserGameInputDto.java
+++ backend/src/main/java/com/ssafy/backend/dto/UserGameInputDto.java
@@ -2,11 +2,11 @@ package com.ssafy.backend.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class UserGameInputDto {
-    
+    //todo front에서 받아올 데이터들
 }

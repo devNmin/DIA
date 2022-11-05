980fd1f4d6904af119a75f56b850a6c61415ffdc
seungh <seungh1024@naver.com>
2022-11-05 19:54:10 +0900

    [FEAT] user game Controller
    사용자 개인 게임 정보 API 추가
    Querydsl 추가

--- backend/src/main/java/com/ssafy/backend/service/UserGameService.java
+++ backend/src/main/java/com/ssafy/backend/service/UserGameService.java
@@ -1,5 +1,7 @@
 package com.ssafy.backend.service;
 
+import com.ssafy.backend.dto.UserGameDto;
+import com.ssafy.backend.entity.UserGame;
 import com.ssafy.backend.repository.UserGameRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,16 @@ public class UserGameService {
 
     public UserGameService(UserGameRepository userGameRepository) {
         this.userGameRepository = userGameRepository;
+    }
+
+    // game_id + user_id 로 해당 게임 정보를 찾아옴
+    public UserGameDto getUserGame(Long userId, Long gameId){
+        UserGameDto userGame = userGameRepository.getUserGameInfo(userId,gameId);
+
+        if(userGame != null){
+            return userGame;
+        }else{
+            return null;
+        }
     }
 }

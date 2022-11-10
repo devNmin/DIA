7068b7fa772092b906d9193ac4014d5d5da2f223
hyeoklimKwon <khlzzang@daum.net>
2022-11-10 21:45:18 +0900

    [FEAT] teamregister

--- frontend/src/pages/TeamRegister.js
+++ frontend/src/pages/TeamRegister.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React ,{useContext, useState } from 'react'
 import FootballNavbar from '../components/Navbar/FootballNavbar'
+import UserContext from '../context/UserContext';
+import styles from './TeamRegister.module.css'
 
 export default function TeamRegister() {
+  const {ourColor, setOurColor, theirColor, setTheirColor} = useContext(UserContext) 
+  const testColor = (e) => {
+    console.log(e);
+  }
+
   return (
     <div>
-        <FootballNavbar currentpage = 'teamregister'></FootballNavbar>
-        <div>
-            this is team register page
-        </div>      
+        <FootballNavbar currentpage = 'teamregister'></FootballNavbar>   
+        <div className= {styles.rightUx}>
+          <div className={styles.teamColorCon}>
+            <div className= {styles.teamText}>
+              <p className = {styles.colorTitle}> Home Team Color :  </p>
+              <input type= 'color'
+              className= {styles.colorInput}
+              value= {ourColor}
+              onChange={(e) => setOurColor(e.target.value)}          
+              />     
+            </div>
+            <div className= {styles.teamText}>
+            <p className = {styles.colorTitle}> Away Team Color  : </p>
+              <input type= "color"
+              className= {styles.colorInput}
+              value= {theirColor}
+              onChange={(e) => setTheirColor(e.target.value)}          
+              />     
+            </div>
+          </div>
+          <div className= {styles.startingCon}>
+            HI
+          </div>   
+
+        </div>
+        
     </div>
   )
 }

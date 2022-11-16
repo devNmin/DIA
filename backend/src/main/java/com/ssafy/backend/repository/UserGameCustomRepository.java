ff22e562fc6c9fc302738ba40cf9e4b258c2d20a
seungh <seungh1024@naver.com>
2022-11-16 17:49:37 +0900

    [REFACTOR] game stat
    mypage 게임 통계 스탯에 사용되는 쿼리문 QueryDSL로 수정

--- backend/src/main/java/com/ssafy/backend/repository/UserGameCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/UserGameCustomRepository.java
@@ -2,9 +2,13 @@ package com.ssafy.backend.repository;
 
 import com.ssafy.backend.dto.UserGameDto;
 import com.ssafy.backend.entity.UserGame;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserGameCustomRepository {
     UserGameDto getUserGameInfo(Long userId, Long gameId);
 
+    List<UserGameDto> getMyGameInfo(@Param(value="start")Integer start, @Param(value="end") Integer end , @Param(value="id") Long id);
 
 }
--- backend/src/main/java/com/ssafy/backend/repository/UserGameCustomRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/backend/repository/UserGameCustomRepositoryImpl.java
@@ -3,10 +3,12 @@ package com.ssafy.backend.repository;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.backend.dto.UserGameDto;
+import com.ssafy.backend.entity.QGame;
 import com.ssafy.backend.entity.QUserGame;
 import com.ssafy.backend.entity.UserGame;
 
 import javax.persistence.Column;
+import java.util.List;
 
 public class UserGameCustomRepositoryImpl implements UserGameCustomRepository{
     private final JPAQueryFactory jpaQueryFactory;
@@ -36,5 +38,32 @@ public class UserGameCustomRepositoryImpl implements UserGameCustomRepository{
                         qUserGame.userPhysical))
                 .from(qUserGame)
                 .where(qUserGame.user.userId.eq(userId).and(qUserGame.game.gameId.eq(gameId))).fetchOne();
+    }
+
+    @Override
+    public List<UserGameDto> getMyGameInfo(Integer start, Integer end, Long id) {
+        QUserGame qUserGame = new QUserGame("userGame");
+        QGame qGame = new QGame("game");
+        return jpaQueryFactory.select(Projections.constructor(UserGameDto.class,
+                        qUserGame.usergameId,
+                        qUserGame.userAttack,
+                        qUserGame.userDefence,
+                        qUserGame.userDistance,
+                        qUserGame.userGoal,
+                        qUserGame.userHeart,
+                        qUserGame.userHeatmap,
+                        qUserGame.userMaxSpeed,
+                        qUserGame.userPass,
+                        qUserGame.userPosition,
+                        qUserGame.userSave,
+                        qUserGame.userSpeed,
+                        qUserGame.userSprint,
+                        qUserGame.userStamina,
+                        qUserGame.userPhysical))
+                .from(qUserGame)
+                .leftJoin(qGame)
+                .on(qUserGame.game.gameId.eq(qGame.gameId))
+                .where(qUserGame.user.userId.eq(id))
+                .orderBy(qGame.gameYear.desc(),qGame.gameMonth.desc(),qGame.gameDay.desc()).fetch();
     }
 }

9b1fc846be7ed49d2898525e0db479129d71323b
kimjinsan <lazybones1@naver.com>
2022-11-19 03:38:13 +0900

    [FEAT] 그래프 크기 조정
    그래프 마지막 phy의 tooltip이 생기면 사이즈가 이상해 지는 오류 발견
    원인 파악중
    right를 크게 하니 해당 오류는 발생하지 않음

--- frontend/src/components/MyPage/DataGraphDetail.js
+++ frontend/src/components/MyPage/DataGraphDetail.js
@@ -9,7 +9,7 @@ function DataGraphDetail({data}) {
     <div className={styles.body}>
       <ResponsiveLine
         data={data}
-        margin={{ top: 10, right: 100, bottom: 50, left: 60 }}
+        margin={{ top: 10, right: 150, bottom: 50, left: 60 }}
         xScale={{ type: 'point' }}
         yScale={{
             type: 'linear',
@@ -102,7 +102,6 @@ function DataGraphDetail({data}) {
                 background: '#ffffff',
                 color: '#333333',
                 fontSize: 16,
-                alignContent:'start'
               },
             },
           }}

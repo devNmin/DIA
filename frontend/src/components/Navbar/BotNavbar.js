9ae7ea349f4b192c465dd3e31d3bbbf029e97bb8
kimjinsan <lazybones1@naver.com>
2022-11-21 08:36:18 +0900

    [FEAT] 색상 수정

--- frontend/src/components/Navbar/BotNavbar.js
+++ frontend/src/components/Navbar/BotNavbar.js
@@ -1,15 +1,36 @@
 // import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import React, {  useState, useContext, useEffect } from 'react';
 // import AuthContext from '../../context/AuthContext';
 import styles from './BotNavbar.module.css'
+import axios from 'axios';
+import AuthContext from '../../context/AuthContext';
 
 function BotNavbar(props) {
+  let { authTokens } = useContext(AuthContext);
   const history = useHistory();
+  const [userInfo, setUserInfo] = useState(null);
   // let { user} = useContext(AuthContext)
-
+  const getUserInfo = () => {
+    axios({
+      headers: {
+        Authorization: `Bearer ${authTokens.accessToken}`,
+      },
+      url: 'https://k7b307.p.ssafy.io/api/v1/user/info',
+      method: 'GET',
+    })
+      .then((response) => {
+        // console.log('여깅', response);
+        setUserInfo(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
   const handleHistory = () => {
     history.goBack();
   }
+  useEffect(() => {
+    getUserInfo();
+  }, []);
   return (
     <div className={styles.wrapper}>
       <div>
@@ -20,7 +41,7 @@ function BotNavbar(props) {
       </div>
         <div>
           <button className={styles.navButton}>
-            Hello, young
+            Hello,{userInfo?.userName}
           </button>
         </div>
     </div>
